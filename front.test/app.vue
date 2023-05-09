7ffb7e7a5da02e13ec2e6ec2f15f407b8c4eb919
heojun <heojun@cowave.kr>
2023-05-09 16:01:41 +0900

    feat: 아티클 컴포넌트로 분리

--- front.test/app.vue
+++ front.test/app.vue
@@ -1,92 +1,3 @@
-<script setup>
-
-class ArticleModel {
-    constructor() {
-        this.API_URI = 'http://back.test'
-    }
-    async get() {
-        const { data } = await useAsyncData(() => $fetch(`${this.API_URI}/articles`))
-        return data
-    }
-    async add(postData) {
-        await useAsyncData(() => $fetch(`${this.API_URI}/articles`, {
-            method: 'post',
-            body: postData
-        }))
-    }
-    async edit(id, postData) {
-        await useAsyncData(() => $fetch(`${this.API_URI}/articles/${id}`, {
-            method: 'put',
-            body: postData
-        }))
-    }
-    async delete(id) {
-        await useAsyncData(() => $fetch(`${this.API_URI}/articles/${id}`, {
-            method: 'delete',
-        }))
-    }
-}
-
-class AritcleView {
-    reset() {
-        editMode.value = true
-        title.value = '';
-        content.value = '';
-    }
-}
-
-class ArticleController {
-    constructor() {
-        this.articleModel = new ArticleModel()
-        this.aritcleView = new AritcleView()
-    }
-    async get() {
-        return this.articleModel.get()
-    }
-    async add() {
-        const postData = {
-            title: title.value,
-            content: content.value,
-        }
-        await this.articleModel.add(postData)
-        await this.get()
-        this.reset()
-    }
-    async edit() {
-        const postData = {
-            title: title.value,
-            content: content.value,
-        }
-        await this.articleModel.edit(id.value, postData)
-        await this.get()
-        this.reset()
-    }
-    async delete() {
-        await this.articleModel.delete(id.value)
-        await this.get()
-        this.reset()
-    }
-    onClick(index) {
-        editMode.value = false
-        const article = articles.value[index]
-        title.value = article.title
-        content.value = article.content
-        id.value = article.id
-    }
-    reset() {
-        this.aritcleView.reset()
-    }
-}
-const articleController = new ArticleController()
-const articles = await articleController.get()
-
-let title = ref('')
-let content = ref('')
-let id = ref('')
-let editMode = ref(true)
-
-</script>
-
 <template>
     <div>
         <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
@@ -111,7 +22,7 @@ let editMode = ref(true)
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">
                                     <span data-feather="home"></span>
-                                    Dashboard
+                                    Board
                                 </a>
                             </li>
                             <li class="nav-item">
@@ -183,74 +94,13 @@ let editMode = ref(true)
                 </nav>
 
                 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    <div
-                        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 class="h2">Dashboard</h1>
-                        <div class="btn-toolbar mb-2 mb-md-0">
-                            <div class="btn-group me-2">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-                            </div>
-                            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                                <span data-feather="calendar"></span>
-                                This week
-                            </button>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control title" placeholder="title" v-model="title">
-                            <label>title</label>
-                        </div>
-                        <div class="form-floating">
-                            <textarea class="form-control content" placeholder="content" v-model="content"
-                                style="height: 100px"></textarea>
-                            <label>content</label>
-                        </div>
-                        <div style="margin-top:3px;">
-                            <button type="button" class="btn btn-sm btn-success mar3" @click="articleController.reset"
-                                v-if="!editMode">Cancel</button>
-                            <button type="button" class="btn btn-sm btn-primary mar3" @click="articleController.add"
-                                v-if="editMode">Add</button>
-                            <button type="button" class="btn btn-sm btn-primary mar3" @click="articleController.edit"
-                                v-if="!editMode">Edit</button>
-                            <button type="button" class="btn btn-sm btn-danger mar3" @click="articleController.delete"
-                                v-if="!editMode">Delete</button>
-                        </div>
-                    </div>
-
-                    <h2>Section title</h2>
-                    <div class="table-responsive">
-                        <table class="table table-striped table-sm articleList">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">title</th>
-                                    <th scope="col">content</th>
-                                    <th scope="col">userId</th>
-                                    <th scope="col">createdAt</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="(article, index) in articles" :key="article.id"
-                                    @click="articleController.onClick(index)">
-                                    <td>{{ article.id }}</td>
-                                    <td>{{ article.title }}</td>
-                                    <td>{{ article.content }}</td>
-                                    <td>{{ article.userId }}</td>
-                                    <td>{{ article.createdAt }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <ArticleComponent />
                 </main>
             </div>
         </div>
 
     </div>
 </template>
-
 
 <style scoped>
 body {
@@ -369,14 +219,5 @@ body {
     .bd-placeholder-img-lg {
         font-size: 3.5rem;
     }
-}
-</style>
-<style scoped>
-.articleList tbody td {
-    cursor: pointer;
-}
-
-.mar3 {
-    margin-right: 3px;
 }
 </style>
---
+++ front.test/components/ArticleComponent.vue
@@ -0,0 +1,166 @@
+<script setup>
+
+class ArticleModel {
+    constructor() {
+        this.API_URI = 'http://back.test'
+    }
+    async get() {
+        const { data } = await useAsyncData(() => $fetch(`${this.API_URI}/articles`))
+        return data
+    }
+    async add(postData) {
+        await useAsyncData(() => $fetch(`${this.API_URI}/articles`, {
+            method: 'post',
+            body: postData
+        }))
+    }
+    async edit(id, postData) {
+        await useAsyncData(() => $fetch(`${this.API_URI}/articles/${id}`, {
+            method: 'put',
+            body: postData
+        }))
+    }
+    async delete(id) {
+        await useAsyncData(() => $fetch(`${this.API_URI}/articles/${id}`, {
+            method: 'delete',
+        }))
+    }
+}
+
+class AritcleView {
+    reset() {
+        editMode.value = true
+        title.value = '';
+        content.value = '';
+    }
+}
+
+class ArticleController {
+    constructor() {
+        this.articleModel = new ArticleModel()
+        this.aritcleView = new AritcleView()
+    }
+    async get() {
+        return this.articleModel.get()
+    }
+    async add() {
+        const postData = {
+            title: title.value,
+            content: content.value,
+            userId: 1,
+        }
+        await this.articleModel.add(postData)
+        await this.get()
+        this.reset()
+    }
+    async edit() {
+        const postData = {
+            title: title.value,
+            content: content.value,
+        }
+        await this.articleModel.edit(id.value, postData)
+        await this.get()
+        this.reset()
+    }
+    async delete() {
+        await this.articleModel.delete(id.value)
+        await this.get()
+        this.reset()
+    }
+    onClick(index) {
+        editMode.value = false
+        const article = articles.value[index]
+        title.value = article.title
+        content.value = article.content
+        id.value = article.id
+    }
+    reset() {
+        this.aritcleView.reset()
+    }
+}
+
+let title = ref('')
+let content = ref('')
+let id = ref('')
+let editMode = ref(true)
+
+const articleController = new ArticleController()
+const articles = await articleController.get()
+
+</script>
+
+
+<template>
+    <div>
+        <div
+            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Dashboard</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+                <div class="btn-group me-2">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+                </div>
+                <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                    <span data-feather="calendar"></span>
+                    This week
+                </button>
+            </div>
+        </div>
+
+        <div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control title" placeholder="title" v-model="title">
+                <label>title</label>
+            </div>
+            <div class="form-floating">
+                <textarea class="form-control content" placeholder="content" v-model="content"
+                    style="height: 100px"></textarea>
+                <label>content</label>
+            </div>
+            <div style="margin-top:3px;">
+                <button type="button" class="btn btn-sm btn-success mar3" @click="articleController.reset"
+                    v-if="!editMode">Cancel</button>
+                <button type="button" class="btn btn-sm btn-primary mar3" @click="articleController.add"
+                    v-if="editMode">Add</button>
+                <button type="button" class="btn btn-sm btn-primary mar3" @click="articleController.edit"
+                    v-if="!editMode">Edit</button>
+                <button type="button" class="btn btn-sm btn-danger mar3" @click="articleController.delete"
+                    v-if="!editMode">Delete</button>
+            </div>
+        </div>
+
+        <h2>Section title</h2>
+        <div class="table-responsive">
+            <table class="table table-striped table-sm articleList">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">title</th>
+                        <th scope="col">content</th>
+                        <th scope="col">userId</th>
+                        <th scope="col">createdAt</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(article, index) in articles" :key="article.id" @click="articleController.onClick(index)">
+                        <td>{{ article.id }}</td>
+                        <td>{{ article.title }}</td>
+                        <td>{{ article.content }}</td>
+                        <td>{{ article.userId }}</td>
+                        <td>{{ article.createdAt }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.articleList tbody td {
+    cursor: pointer;
+}
+
+.mar3 {
+    margin-right: 3px;
+}
+</style>
