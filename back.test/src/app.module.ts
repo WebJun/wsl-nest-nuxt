111a713f663d8a4da8cbf97c8d77ecfe2a695a21
heojun <heojun@cowave.kr>
2023-05-12 14:16:32 +0900

    feat: .env에 DB 설정하게 변경

--- back.test/src/app.module.ts
+++ back.test/src/app.module.ts
@@ -1,27 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { Article } from './articles/article.entity';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { getConfig, getDataSourceConfig } from './common/database/configuration';
 import { ArticlesModule } from './articles/articles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'back.db',
-      port: 3306,
-      username: 'root',
-      password: 'docker123',
-      database: 'nuxt',
-      entities: [Article],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env`,
+      ignoreEnvFile: false,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => getConfig(),
+      dataSourceFactory: async function () {
+        return addTransactionalDataSource(new DataSource(getDataSourceConfig()));
+      },
     }),
     ArticlesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
