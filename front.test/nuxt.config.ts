a7a40ce6265cad61377c15da6e2306883ea98208
heojun <heojun@cowave.kr>
2023-05-18 10:38:18 +0900

    feat: 라우팅 추가

--- front.test/nuxt.config.ts
+++ front.test/nuxt.config.ts
@@ -1,17 +1,37 @@
+import { NuxtPage } from 'nuxt/schema';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     //   modules: ['@nuxt/ui'],
     css: ['bootstrap/dist/css/bootstrap.css'],
     //   script: [{ src: 'bootstrap/dist/js/bootstrap.min.js', body: true }],
-    router: {
-        options: {
-            routes: () => [
-                {
-                    name: 'aaa',
-                    path: '/aaa',
-                    component: () => import('~/pages/about.vue'),
-                },
-            ],
+    hooks: {
+        'pages:extend'(pages) {
+            // add a route
+            pages.push({
+                name: 'articles',
+                path: '/',
+                file: '~/pages/articles.vue',
+            });
+            // remove routes
+            function removePagesMatching(
+                pattern: RegExp,
+                pages: NuxtPage[] = []
+            ) {
+                const pagesToRemove = [];
+                console.log(pages);
+                for (const page of pages) {
+                    if (pattern.test(page.file as string)) {
+                        pagesToRemove.push(page);
+                    } else {
+                        removePagesMatching(pattern, page.children);
+                    }
+                }
+                for (const page of pagesToRemove) {
+                    pages.splice(pages.indexOf(page), 1);
+                }
+            }
+            removePagesMatching(/\.ts$/, pages);
         },
     },
 });
